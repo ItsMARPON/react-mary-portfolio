6b832a533c57b6134b2fae738387be2de44c795b
Mary <itsmaryyang@gmail.com>
2023-04-29 11:26:11 -0500

    Add onclick functions for Navbar, finish conditional render function in PortfolioContainer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,22 +2,18 @@ import Nav from 'react-bootstrap/Nav';
 
 function Navbar({currentPage, handlePageChange}) {
   return (
-    <Nav variant="tabs" defaultActiveKey="/home">
+    <Nav variant="tabs" defaultActiveKey="/abouthome">
       <Nav.Item>
-        <Nav.Link href="/home" onClick={() => handlePageChange('Home')}>Home</Nav.Link>
+        <Nav.Link href="/abouthome" onClick={() => handlePageChange('AboutHome')}>Home:About</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="/portfolio">Portfolio</Nav.Link>
+        <Nav.Link eventKey="/portfolio" onClick={() => handlePageChange('Portfolio')}>Portfolio</Nav.Link>
+        </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="/contact" onClick={() => handlePageChange('Contact')}>Contact</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="disabled">
-        Resume
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="disabled">
-        Resume
-        </Nav.Link>
+        <Nav.Link eventKey="/resume" onClick={() => handlePageChange('Resume')}>Resume</Nav.Link>
       </Nav.Item>
     </Nav>
   );
--- src/pages/PortfolioContainer.js
+++ src/pages/PortfolioContainer.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import {NavTabs} from "react-bootstrap";
-import About from "./About";
+import AboutHome from "./AboutHome";
 import Contact from "./Contact";
 import Portfolio from "./Portfolio";
 import Resume from "./Resume";
@@ -9,22 +9,20 @@ function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState("Home");
 
   const renderPage = () => {
-    if (currentPage === "Home") {
-      return <Home />;
+    if (currentPage === "AboutHome") {
+      return <AboutHome />;
     }
-    if (currentPage === "About") {
-      return <About />;
+    if (currentPage === "Portfolio") {
+      return <Portfolio />;
     }
     if (currentPage === "Contact") {
       return <Contact />;
     }
-    if (currentPage === "P") {
-        return <Contact />;
-      }
     return <Resume />;
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
+  
   return (
     <div>
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
