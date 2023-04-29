e8d7a6578bef5b77a4c590199bf8796635f5de72
Mary <itsmaryyang@gmail.com>
2023-04-29 11:29:06 -0500

    Add About Me details

--- src/pages/AboutHome.js
+++ src/pages/AboutHome.js
@@ -1,1 +1,14 @@
 import React from 'react';
+
+export default function AboutHome(){
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>
+                Graduated from St. Catherine University with a degree in Accounting. I worked in Tax and Accounting Operations for most of my career. My most recent job was with the State of Minnesota working in the Accounting Division and handling the financial operations for the State. During this time, I gained experience with using Oracle database structures and Microsoft Access Database to maintain and build systems for the operations team. During my time, the State's Financial Accounting system had to undergo a large upgrade that would essentially change the user's interaction with the system. I assisted in writing test scripts and with testing the functional aspects of the new system. It was during this time, that I discovered a passion for working on technical issues and aiding in the solution and implemenation of the upgrade. 
+
+                Knowing that I had a real passion for the technology side, I enrolled into the University of Minnesota Full-stack Web Development Bootcamp. Once I finish the bootcamp, I hope to have a strong foundation for beginning a career in web development.
+            </p>
+        </div>
+    );
+};
