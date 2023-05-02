f5b9032930ab8522cff21b503ab62959c31810d6
Mary <itsmaryyang@gmail.com>
2023-05-02 10:40:42 -0500

    Add Footer Code

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../index.css';
+
+function Footer(){
+    return (
+        <footer className='footer'>
+            <h3>Copyright (c) by Mary Yang</h3>
+        </footer>
+    )
+    
+}
+
+export default Footer;
