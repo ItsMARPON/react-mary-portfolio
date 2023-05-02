495654d7f511f7236e33315b7d3a41501273f39d
Mary <itsmaryyang@gmail.com>
2023-05-02 10:38:48 -0500

    Add Header code

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../index.css';
+
+function Header(){
+    return (
+        <header className='header'>
+            <h1>Professional Portfolio</h1>
+        </header>
+    );
+};
+
+export default Header;
+
