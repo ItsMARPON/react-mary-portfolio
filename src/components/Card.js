cbd6017cf3fbe6368d66478cf8601b063e4ec6dd
Mary <itsmaryyang@gmail.com>
2023-05-02 11:01:48 -0500

    add CSS styling to index.css

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../index.css';
+
+function Card() {
+  return (
+    <div className="card-container">
+      <div className="card-title">Project 1</div>
+      <div className="card-desc">
+        <span>This is description of card</span>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
