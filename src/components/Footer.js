0547a5e2a1f68a25cbfef88824f1a79f1f8092f3
Mary <itsmaryyang@gmail.com>
2023-05-02 15:23:45 -0500

    Update index.css for Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../index.css';
 
 function Footer(){
     return (
         <footer className='footer'>
-            <h3>Copyright (c) by Mary Yang</h3>
+            <div className='copyright'>
+                <h3>Copyright (c) by Mary Yang</h3>
+            </div>
+            <div className='githubIcon'>
+            <FontAwesomeIcon icon="fa-brands fa-github" />
+            </div>
+            <div className='linkedinIcon'>
+            <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+            </div>
         </footer>
     )
     
