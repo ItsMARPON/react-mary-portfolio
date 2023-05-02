30b02ac80fbd51186f87931fd9cb6fea7c6404b6
Mary <itsmaryyang@gmail.com>
2023-05-02 16:34:08 -0500

    Add LinkedIn and Github profile links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../index.css';
 
 function Footer(){
     return (
         <footer className='footer'>
-            <div className='copyright'>
-                <h3>Copyright (c) by Mary Yang</h3>
-            </div>
             <div className='githubIcon'>
             <FontAwesomeIcon icon="fa-brands fa-github" />
+                <Link to="/github">
+                    <img src ="https://github.com/ItsMARPON" alt ="Github site"/>
+                </Link>
             </div>
             <div className='linkedinIcon'>
             <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+            <Link to="/linkedin">
+                <img src='https://www.linkedin.com/in/maryyangytio' alt='LinkedIn site' />
+            </Link>
+            </div>
+            <div className='copyright'>
+                <h3>Copyright (c) by Mary Yang</h3>
             </div>
         </footer>
     )
