c4f96d705d10e0aaa66b29289d8dfa8bfcc342e4
Mary <itsmaryyang@gmail.com>
2023-05-02 10:31:29 -0500

    Link components and pages to App.js and update  app.css styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import pic from './profile.png';
+import PortfolioContainer from "./pages/PortfolioContainer";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={pic} className="App-pic" alt="profile-pic" />
       </header>
+      <Navbar />
+      <PortfolioContainer />
+      <Footer />
     </div>
   );
 }
