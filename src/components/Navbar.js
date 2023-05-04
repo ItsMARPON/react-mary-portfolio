cfdd6f75547387a180ae08e7cecfd58fdd02ebdd
Mary <itsmaryyang@gmail.com>
2023-05-03 19:07:45 -0500

    update Navbar.js tab to Home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar({currentPage, handlePageChange}) {
   return (
     <Nav variant="tabs" defaultActiveKey="/abouthome">
       <Nav.Item>
-        <Nav.Link href="/abouthome" onClick={() => handlePageChange('AboutHome')}>Home:About</Nav.Link>
+        <Nav.Link href="/abouthome" onClick={() => handlePageChange('Home')}>Home</Nav.Link>
       </Nav.Item>
       <Nav.Item>
         <Nav.Link eventKey="/portfolio" onClick={() => handlePageChange('Portfolio')}>Portfolio</Nav.Link>
