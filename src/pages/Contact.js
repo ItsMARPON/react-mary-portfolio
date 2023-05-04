2b39181a9c1e06c054691079e9438770e2991701
Mary <itsmaryyang@gmail.com>
2023-05-03 22:16:00 -0500

    Add placeholders and button to Contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,15 +5,15 @@ function Contact() {
     <Form className='form-container'>
       <Form.Group className="mb-3" controlId="form-name">
         <Form.Label>Your Full Name</Form.Label>
-        <Form.Control type="name" placeholder="first middle last name" />
+        <Form.Control type="name" placeholder="First Middle Last" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="form-email">
-        <Form.Label>Email address</Form.Label>
+        <Form.Label>Email:</Form.Label>
         <Form.Control type="email" placeholder="name@example.com" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="form-Textarea1">
-        <Form.Label>textarea</Form.Label>
-        <Form.Control as="textarea" rows={3} />
+        <Form.Label>Comments:</Form.Label>
+        <Form.Control as="textarea" rows={3} placeholder='Feedback or Questions are appreciated'/>
         <button className='btn btn-primary'>Submit</button>
       </Form.Group>
     </Form>
